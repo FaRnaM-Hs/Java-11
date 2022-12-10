77507f0cae344020e0d90b64462706c5554d8b06
Farnam <farnam.hs@yahoo.com>
2022-12-10 13:44:18 +0330

    Gson

--- src/test/java/http/HttpClientShould.java
+++ src/test/java/http/HttpClientShould.java
@@ -1,6 +1,10 @@
 package http;
 
+import com.google.gson.Gson;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import player.Player;
+import player.Players;
 
 import java.io.IOException;
 import java.net.URI;
@@ -8,6 +12,10 @@ import java.net.URISyntaxException;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.*;
 
 public class HttpClientShould {
 
@@ -17,7 +25,14 @@ public class HttpClientShould {
                 .uri(new URI("https://javafarnam.free.beeceptor.com/players")).build();
         HttpResponse<String> response = HttpClient.newHttpClient()
                 .send(request, HttpResponse.BodyHandlers.ofString());
-
         System.out.println(response.body());
+
+        Gson gson = new Gson();
+        List<Player> players = gson.fromJson(response.body(), Players.class).getPlayers();
+
+        List<Player> topScorers = players.stream()
+                .filter(player -> player.getGoal() > 225)
+                .collect(Collectors.toUnmodifiableList());
+        assertThat(topScorers).containsExactly(new Player("Ali Karimi", 250));
     }
 }
